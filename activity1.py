bb12e67695d7883a7b42608eab811eb9be1acf73
Muhammadt366 <muhammadbinm22@gamil.com>
2026-07-08 20:18:17 +0500

    python

---
+++ activity1.py
@@ -0,0 +1,20 @@
+name = "Muhammad"
+age = 10
+is_student = True
+weight = 30.5
+ 
+print("Name:", name)
+print("Age:", age)
+print("Is Student:", is_student)
+print("Weight:", weight)
+print("data type of name:", type(name))
+print("data type of age:", type(age))
+print("data type of is_student:", type(is_student))
+print("data type of weight:", type(weight))
+print("/n after type casting.....")
+age=str(age)
+print(age)
+print("data type age is:", type(age))
+weight=int(weight)
+print(weight)
+print("data type weight is:", type(weight))
