08a351ca21e1fa0e059ca726984fb7cf8dfba786
Muhammadt366 <muhammadbinm22@gamil.com>
2026-07-08 20:40:25 +0500

    python

---
+++ activity2.py
@@ -0,0 +1,21 @@
+
+num1 = 45
+num2 = 3
+print("number 1:", num1)
+print("number 2:", num2)
+print("Addition:", num1 + num2)
+print("difference:", num1 - num2)
+print("Product:", num1 * num2)
+print("Division:", num1 / num2)
+print("floor Division:", num1 // num2)
+print("Modulus:", num1 % num2)
+print("square:", num1 ** num2)
+print("square root:", num1 ** 0.5)
+
+print("equal?", num1 == num2)
+print("number 1 greater?", num1 > num2)
+print("number 2 greater?", num1 < num2)
+print("not equal?",num1!=num2)
+
+result = num1/2+num2**2+10
+print("result of given equation is:", result)
